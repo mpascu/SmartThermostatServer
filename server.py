aa98d54bf7d3f8bd6f6cad93352c1625dbdb642f
Marc Pascual Terron <marcpascu@gmail.com>
2015-03-06 10:49:34 +0000

    Added "name" atribute at thermostats, POST methods now retrieve "name", data structucture slightly adapted

--- server.py
+++ server.py
@@ -6,27 +6,27 @@ def hello():
     """Brief introduction message"""
     return "Hello this is the API server of a smart thermostate!"
 
-@app.route('/temp', methods=['GET','PUT','POST'])
+@app.route('/temp', methods=['GET','DELETE','POST'])
 def showTemp():
     """Offers the three available methods of the api for the temperature sensors
     GET - Lists all the sensors values
     POST - Adds a new temperature sensor
-    PUT - Delete all sensors
+    DELETE - Delete all sensors
     """
     global data
     if request.method == 'GET':
         return json.dumps(data['sensors'], indent=4)
-    if request.method == 'PUT':
-        data['sensors'].clear()
+    if request.method == 'DELETE':
+        data['sensors'] = []
         file = open('testData.json','w')
         json.dump(data,file,indent=4)
         file.close()
         return "All sensors deleted successfully"
     if request.method == 'POST':
         id = len(data['sensors'])+1
-        temp= {str(id) : {"value":"0"}}		
-        data['sensors'].update(temp)
-        file = open('temps.json','w')
+        temp= {str(id) : {"value":"0", "name":request.form['name']}}		
+        data['sensors'].append(temp)
+        file = open('testData.json','w')
         json.dump(data,file,indent=4)
         file.close()
         return "New temperature value created successfully"
@@ -49,17 +49,17 @@ def showThermo():
     """
     global data
     if request.method == 'GET':
-        return json.dumps(data['thermostates'], indent=4) 
+        return json.dumps(data['thermostats'], indent=4) 
     if request.method == 'POST':
-        id = len(data['thermostates'])+1
-        thermo= {str(id) : {'sensors':[], 'temperature':'21'}}		
-        data['thermostates'].update(thermo)
+        id = len(data['thermostats'])+1
+        thermo= {str(id) : {"name":request.form['name'], 'sensors':[], 'temperature':'21'}}		
+        data['thermostats'].append(thermo)
         file = open('testData.json','w')
         json.dump(data,file,indent=4)
         file.close()
         return "New thermostate created successfully"
     if request.method == 'DELETE':
-        data['thermostates'].clear()
+        data['thermostats']=[]
         file = open('testData.json','w')
         json.dump(data,file,indent=4)
         file.close()
